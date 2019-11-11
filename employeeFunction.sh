b50717341a1ea6b218c449658c77097751325a15
Admin <zafaraabid3295@gmail.com>
2019-11-11 11:15:51 +0530

    update file

---
+++ employeeFunction.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+function myfunc() {
+isFullTime=1;
+isPartTime=2;
+rate=100;
+hours=0;
+parDaySalary=0;
+salary=0;
+for (( counter=1; counter<=20; counter++ ))
+do
+    randomValue=$(( RANDOM % 3 )) 
+     if [ $randomValue == $isFullTime ]
+then
+    
+    echo "Full Time"
+    hours=9;
+ 
+
+elif [ $randomValue == $isPartTime ]
+ then
+  
+   hours=4;
+
+else
+   hours=0;
+  
+fi
+parDaySalary=$(( $parDaySalary +  $rate * $hours )) 
+
+done
+echo $parDaySalary
+}
+
+result="$( myfunc )"
+echo $result
+
