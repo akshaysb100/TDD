be03848047947d96fb9f9d335b3be9297b790fd2
Admin <zafaraabid3295@gmail.com>
2019-11-11 10:24:44 +0530

    new for loop file

---
+++ forLoopNew.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+isFullTime=1;
+isPartTime=2;
+rate=100;
+hours=0;
+parDaySalary=0;
+salary=0;
+for (( counter=1; counter<=20; counter++ ))
+do
+    randomValue=$(( RANDOM % 3 )) 
+     if [ $randomValue == $isFullTime ]
+then
+    
+    echo "Full Time job" 
+    hours=9;
+ 
+
+elif [ $randomValue == $isPartTime ]
+ then
+  
+   hours=4;
+
+else
+   hours=0;
+  
+fi
+parDaySalary=$(( $parDaySalary +  $rate * $hours )) 
+
+done
+salary=$parDaySalary;
+printf "\n"
